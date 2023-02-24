ddb87f3ff87cd4035c733c952590a5e5679b2363
Alexander Gordey <alexnickgordey3@gmail.com>
2023-02-24 09:59:26 +0300

    записываю на ветку 2

--- PycharmProjects/zadachi_codwars/main.py
+++ PycharmProjects/zadachi_codwars/main.py
@@ -1,11 +1,8 @@
 
-ls = [8,3,11,15]
-n = int(input())
 
-lstotal = []
 
-for c in range(len(ls) - 1):
-    if ls[c] +ls[c + 1 ] == n:
-        lstotal.append(c)
-        lstotal.append(c + 1)
-print(lstotal)
+a = int(input())
+c = a **2
+
+print(c)
+
