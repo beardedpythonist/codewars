99763c10df3c0c3501892dc0029507fa07d64603
Alexander Gordey <alexnickgordey3@gmail.com>
2023-02-24 10:09:23 +0300

    записываю на ветку 4  уже запутался.  как найти изначальные файлы???

--- PycharmProjects/zadachi_codwars/main.py
+++ PycharmProjects/zadachi_codwars/main.py
@@ -1,11 +1,9 @@
 
-ls = [8,3,11,15]
-n = int(input())
 
-lstotal = []
+w = {}
 
-for c in range(len(ls) - 1):
-    if ls[c] +ls[c + 1 ] == n:
-        lstotal.append(c)
-        lstotal.append(c + 1)
-print(lstotal)
+w['a'] = 100
+
+print(w)
+
+
